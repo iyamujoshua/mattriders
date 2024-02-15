c375850a995be4a900fefc33d54c6b7f47a867df
Henryford52 <henryuzzi75@gmail.com>
2024-02-15 22:39:33 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import './App.css'
+import Home from './router/home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
+   
+    // ReviewPage
+  ]);
 
   return (
     <>
-     hey
+  <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/router/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Body from '../components/body'
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Body/>
+    </div>
+  )
+}
+
+export default Home
