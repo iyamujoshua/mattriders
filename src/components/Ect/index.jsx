e1d4d11b90dab5992b64365da1c4eae4e8aaa65f
iyamujoshua <joshamu20@gmail.com>
2024-02-17 16:41:11 +0100

    Added erc20 and 250m $SLOT

---
+++ src/components/Ect/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./style.css"
+export default function Ect() {
+  return (
+    <div className='Features'>
+        <div className="Features_flex">
+        <div className="Features_container1">
+          <h3>
+          ERC20
+          </h3>
+          <p>
+          ETH TOKEN
+          </p>
+        </div>
+        <div className="Features_container2">
+          <h3>
+          250M
+          </h3>
+          <p>
+          CIRCULATING SUPPLY
+          </p>
+        </div>
+
+        <div className="Features_container3">
+          <h3>
+          250M
+          </h3>
+          <p>
+          TOKEN UTILITY
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -4,6 +4,7 @@ import Presale from '../Presale'
 import Equity from '../Equity'
 import Verified from '../Verified'
 import Tokenomics from "../Tokenomics"
+import Ect from '../Ect'
 const Body = () => {
   return (
     <div>
@@ -12,6 +13,7 @@ const Body = () => {
         <Equity/>
         <Verified></Verified>
         <Tokenomics/>
+        <Ect/>
     </div>
   )
 }
